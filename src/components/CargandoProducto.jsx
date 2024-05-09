feba81820ff9c1413d2f8bf73f5cd3533304cde5
MiguelLuceroSL <roquerobertomiguellucero@gmail.com>
2024-05-09 16:45:21 -0300

    changesss

--- src/components/CargandoProducto.jsx
+++ src/components/CargandoProducto.jsx
@@ -2,8 +2,7 @@ import React from "react"
 
 const MyLoader = () => {
     return (
-        <article className="relative flex flex-col w-full min-h-screen gap-0 md:gap-4 mt-0 md:mt-6 animate-pulse items-center justify-center">
-            <section className="h-11 w-36 rounded-full bg-slate-400" />
+        <article className="relative flex flex-col w-full min-h-screen gap-4 mt-0 md:mt-6 animate-pulse items-center justify-center">
             <section className="h-[500px] w-64 rounded-3xl bg-slate-400" />
         </article>
     )
